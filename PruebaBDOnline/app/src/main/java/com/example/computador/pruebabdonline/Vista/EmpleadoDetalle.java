ff3bd822b55ff0eeb5e886d97766989219c84b10
cctorres <c.torresalzate@gmail.com>
2017-05-11 21:47:02 -0500

    Empleado detalle

---
+++ PruebaBDOnline/app/src/main/java/com/example/computador/pruebabdonline/Vista/EmpleadoDetalle.java
@@ -0,0 +1,15 @@
+package com.example.computador.pruebabdonline.Vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.computador.pruebabdonline.R;
+
+public class EmpleadoDetalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empleado_detalle);
+    }
+}
